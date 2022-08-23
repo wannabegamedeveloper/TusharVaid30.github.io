2cc06de5daaa160c128782a91ce6fbf0f23e9101
TusharVaid30 <31384470+TusharVaid30@users.noreply.github.com>
2022-08-24 01:54:31 +0530

    WebXR AR

--- app.js
+++ app.js
@@ -1,86 +1,53 @@
-import * as THREE from './libs/three/three.module.js';
-import { VRButton } from './libs/three/jsm/VRButton.js';
-import { XRControllerModelFactory } from './libs/three/jsm/XRControllerModelFactory.js';
-import { BoxLineGeometry } from './libs/three/jsm/BoxLineGeometry.js';
-import { Stats } from './libs/stats.module.js';
-import { OrbitControls } from './libs/three/jsm/OrbitControls.js';
-
+import * as THREE from '../../libs/three125/three.module.js';
+import { OrbitControls } from '../../libs/three125/OrbitControls.js';
+import { Stats } from '../../libs/stats.module.js';
+import { ARButton } from '../../libs/ARButton.js';
 
 class App{
-	constructor(){
-		const container = document.createElement( 'div' );
-		document.body.appendChild( container );
+    constructor(){
+        const container = document.createElement( 'div' );
+        document.body.appendChild( container );
         
         this.clock = new THREE.Clock();
         
-		this.camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 100 );
-		this.camera.position.set( 0, 1.6, 3 );
+        this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
         
-		this.scene = new THREE.Scene();
-        this.scene.background = new THREE.Color( 0x505050 );
-
-		this.scene.add( new THREE.HemisphereLight( 0x606060, 0x404040 ) );
+        this.scene = new THREE.Scene();
+        
+        this.scene.add( new THREE.HemisphereLight( 0x606060, 0x404040 ) );
 
         const light = new THREE.DirectionalLight( 0xffffff );
         light.position.set( 1, 1, 1 ).normalize();
-		this.scene.add( light );
-			
-		this.renderer = new THREE.WebGLRenderer({ antialias: true } );
-		this.renderer.setPixelRatio( window.devicePixelRatio );
-		this.renderer.setSize( window.innerWidth, window.innerHeight );
+        this.scene.add( light );
+            
+        this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
+        this.renderer.setPixelRatio( window.devicePixelRatio );
+        this.renderer.setSize( window.innerWidth, window.innerHeight );
         this.renderer.outputEncoding = THREE.sRGBEncoding;
         
-		container.appendChild( this.renderer.domElement );
+        container.appendChild( this.renderer.domElement );
         
         this.controls = new OrbitControls( this.camera, this.renderer.domElement );
-        this.controls.target.set(0, 1.6, 0);
+        this.controls.target.set(0, 3.5, 0);
         this.controls.update();
         
         this.stats = new Stats();
-        container.appendChild( this.stats.dom );
+        document.body.appendChild( this.stats.dom );
         
         this.initScene();
         this.setupXR();
         
         window.addEventListener('resize', this.resize.bind(this) );
-        
-        this.renderer.setAnimationLoop( this.render.bind(this) );
-	}	
-    
-    random( min, max ){
-        return Math.random() * (max-min) + min;
-    }
+    }   
     
     initScene(){
-        this.radius = 0.08;
-
-        this.room = new THREE.LineSegments(
-                new BoxLineGeometry(6, 6, 6, 10, 10, 10), 
-                new THREE.LineBasicMaterial({color : 0x808080})
-            );
-        this.room.geometry.translate(0, 3, 0);
-        this.scene.add(this.room);
-
-        const geometry = new THREE.IcosahedronBufferGeometry(this.radius, 2);
-
-        for (let i = 0; i < 200; i++)
-        {
-            const object = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial(
-            {
-                color: Math.random() * 0xFFFFFF
-            }));
-
-            object.position.x = this.random(-2, 2);
-            object.position.y = this.random(-2, 2);
-            object.position.z = this.random(-2, 2);
-
-            this.room.add(object);
-        }
+        this.geometry = new THREE.BoxBufferGeometry( 0.06, 0.06, 0.06 ); 
+        this.meshes = [];
     }
     
     setupXR(){
-        this.renderer.xr.enabled = true;
-        document.body.appendChild(VRButton.creatButton(this.renderer));
+        
+        this.renderer.setAnimationLoop( this.render.bind(this) );
     }
     
     resize(){
@@ -89,9 +56,9 @@ class App{
         this.renderer.setSize( window.innerWidth, window.innerHeight );  
     }
     
-	render( ) {   
+    render( ) {   
         this.stats.update();
-        
+        this.meshes.forEach( (mesh) => { mesh.rotateY( 0.01 ); });
         this.renderer.render( this.scene, this.camera );
     }
 }
