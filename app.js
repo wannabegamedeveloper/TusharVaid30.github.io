f1579b3c75b8b21a4e51cedafd11be74e5c12d0c
TusharVaid30 <31384470+TusharVaid30@users.noreply.github.com>
2022-08-24 02:04:40 +0530

    Cube Spawner

--- app.js
+++ app.js
@@ -54,7 +54,13 @@ class App{
 
         function onSelect()
         {
+            const material = new THREE.MeshPhongMaterial({color: 0xFFFFFF * Math.Random()});
 
+            const mesh = new THREE.Mesh(self.geometry, material);
+            mesh.position.set(0, 0, -0.3).applyMatrix4(controller.matrixWorld);
+            mesh.quaternion.setFromRotationMatrix(controller.matrixWorld);
+            self.scene.add(mesh);
+            self.meshes.push(mesh);
         }
 
         const btn = new ARButton(this.renderer);
