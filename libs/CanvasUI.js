c965bfd5d9050990950399d17d036bf1a597e5a1
TusharVaid30 <31384470+TusharVaid30@users.noreply.github.com>
2022-08-24 20:11:56 +0530

    Test 48

--- libs/CanvasUI.js
+++ libs/CanvasUI.js
@@ -36,7 +36,7 @@ class CanvasUI{
             panelSize: { width: 1, height: 1},
             width: 512,
             height: 512,
-            opacity: 0.0,
+            opacity: 0.7,
             body:{
                 fontFamily:'Arial', 
                 fontSize:30, 
